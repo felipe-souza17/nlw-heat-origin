a6c1866c628a43a082e3520f96e149f8c26d5fc0
felipe-souza17 <felipesantos.rsouza@gmail.com>
2021-10-21 19:07:01 -0300

    Light/Dark Mode

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function changeSocialMediaLinks() {
   }
 }
 changeSocialMediaLinks()
+
+turn_mode.addEventListener('click', () => {
+  let bg_card = document.getElementById('bg-card')
+  let link_card = document.getElementById('link-card')
+  let i = document.querySelector('.btn-light')
+
+  body.classList.toggle('background')
+  bg_card.classList.toggle('bg-mode')
+  link_card.classList.toggle('text-mode')
+  if (turn_mode.innerHTML.includes('fa-sun'))
+    turn_mode.innerHTML = '<i class="fas fa-moon"></i>'
+  else turn_mode.innerHTML = '<i class="fas fa-sun"></i>'
+})
