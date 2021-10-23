c6e9d3e278525281743924b77e8d0ca07b9eb2b6
felipe-souza17 <felipesantos.rsouza@gmail.com>
2021-10-23 00:12:41 -0300

    Stage 4 - Working with API

--- main.js
+++ main.js
@@ -14,14 +14,30 @@ function changeSocialMediaLinks() {
 }
 changeSocialMediaLinks()
 
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userImage.src = data.avatar_url
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.src = data.html_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
 turn_mode.addEventListener('click', () => {
   let bg_card = document.getElementById('bg-card')
-  let link_card = document.getElementById('link-card')
+  let userLink = document.getElementById('userLink')
   let i = document.querySelector('.btn-light')
 
   body.classList.toggle('background')
   bg_card.classList.toggle('bg-mode')
-  link_card.classList.toggle('text-mode')
+  userLink.classList.toggle('text-mode')
   if (turn_mode.innerHTML.includes('fa-sun'))
     turn_mode.innerHTML = '<i class="fas fa-moon"></i>'
   else turn_mode.innerHTML = '<i class="fas fa-sun"></i>'
